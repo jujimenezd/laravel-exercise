8bb8c9b446ea264a14569d0c963fe924ff47a1fd
jujimenezd <jujimenezd@unal.edu.co>
2025-06-16 14:57:40 -0500

    create migrations in database

---
+++ database/migrations/2025_06_16_194507_create_notas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_materia');
+            $table->foreign('id_materia')->references('id')->on('materias');
+            $table->unsignedBigInteger('id_estudiante');
+            $table->foreign('id_estudiante')->references('id')->on('users');
+            $table->string('nota');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notas');
+    }
+}
